8837fdf6fe7afd62a331595c6feb74734a76136e
FahimehDan <f.danehdaran@gmail.com>
2025-05-16 16:47:22 +0200

    Event handler in vue js example 1 (counter app)

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 <p>{{greeting}} {{name}}</p>
 <p v-text="question"> </p>
 <p v-html="htmlData" :id="id" :class="id" :style="style"></p>
+<button v-on:click="name='Pierre'">Change le prénom</button>
+
+<!-- Counter App -->
+<h2> {{count}} </h2>
+<button @click="count += 1">Add</button>
+<button @click="count -= 1">Sub</button>
+
 
 </template>
 
@@ -12,6 +19,8 @@ export default {
   data(){
     return{
       name:'Fahimé',
+      count: 0,
+
       greeting:'Salut',
       question:'Comment vas-tu ?',
       htmlData: '<b> Il fait beau !</b>',
